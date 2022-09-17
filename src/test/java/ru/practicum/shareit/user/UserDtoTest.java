d9afe41a21adedde3993b9a5af32016e5765ccd8
Roman Volodin <rozarioagro@gmail.com>
2022-09-17 21:28:07 +0300

    HW done

---
+++ src/test/java/ru/practicum/shareit/user/UserDtoTest.java
@@ -0,0 +1,62 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.practicum.shareit.booking.dto.BookingResponseDto;
+import ru.practicum.shareit.item.dto.CommentResponseDto;
+import ru.practicum.shareit.item.dto.ItemResponseDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+@SpringJUnitConfig({ItemResponseDto.class})
+public class ItemDtoTest {
+
+    @Autowired
+    private JacksonTester<ItemResponseDto> json;
+
+    @Test
+    void testItemDto() throws Exception {
+        var lastBooking = BookingResponseDto.builder().id(1L).itemId(1L).build();
+        var nextBooking = BookingResponseDto.builder().id(2L).itemId(1L).build();
+        var comments = List.of(
+                CommentResponseDto.builder().id(1L).text("text").build(),
+                CommentResponseDto.builder().id(2L).text("text").build()
+        );
+
+        var itemDto = ItemResponseDto.builder()
+                .id(1L)
+                .name("Точилка")
+                .description("Описание")
+                .available(true)
+                .lastBooking(lastBooking)
+                .nextBooking(nextBooking)
+                .ownerId(1L)
+                .requestId(1L)
+                .comments(comments)
+                .build();
+
+        var result = json.write(itemDto);
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE_TIME;
+        String nowTime = now.truncatedTo(ChronoUnit.SECONDS).format(dtf);
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo(itemDto.getName());
+        assertThat(result).extractingJsonPathBooleanValue("$.available").isEqualTo(true);
+        assertThat(result).extractingJsonPathStringValue("$.description").isEqualTo(itemDto.getDescription());
+        assertThat(result).extractingJsonPathValue("$.lastBooking.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathValue("$.nextBooking.id").isEqualTo(2);
+        assertThat(result).extractingJsonPathArrayValue("$.comments").hasSize(2);
+
+    }
+
+}
---
+++ src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -0,0 +1,101 @@
+package ru.practicum.shareit.request;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.storage.ItemRepository;
+import ru.practicum.shareit.requests.RequestMapper;
+import ru.practicum.shareit.requests.dto.ItemRequestResponseDto;
+import ru.practicum.shareit.requests.model.ItemRequest;
+import ru.practicum.shareit.requests.service.ItemRequestService;
+import ru.practicum.shareit.requests.storage.ItemRequestRepository;
+import ru.practicum.shareit.shared.exceptions.ItemNotFoundException;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.storage.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@Import(ValidationAutoConfiguration.class)
+public class RequestServiceTest {
+
+	@Mock
+	private ItemRequestRepository itemRequestRepository;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private ItemRepository itemRepository;
+
+	@Mock
+	private RequestMapper mapper;
+
+	@InjectMocks
+	private ItemRequestService service;
+
+	User firstUser;
+
+	ItemRequest itemRequest;
+
+	Item item;
+
+	ItemRequestResponseDto itemRequestResponseDto;
+
+	@BeforeEach
+	void setUp(){
+		firstUser = User.builder().id(1L).name("First User").build();
+		itemRequest = ItemRequest.builder().id(1L).build();
+		item = Item.builder().id(1L).build();
+		itemRequestResponseDto = ItemRequestResponseDto.builder()
+				.id(1L)
+				.requesterId(1L)
+				.created(LocalDateTime.now())
+				.description("descr")
+				.build();
+	}
+
+	@Test
+	void checkGetRequestByIdThrowException() {
+		when(userRepository.findById(any())).thenReturn(Optional.empty());
+
+		Assertions.assertThrows(ItemNotFoundException.class, () -> service.getRequestByID(1L, 1L));
+	}
+
+	@Test
+	void checkGetRequestByIdItemDoesNotExistThrowException() {
+		when(userRepository.findById(any())).thenReturn(Optional.of(firstUser));
+		when(itemRequestRepository.findById(any())).thenReturn(Optional.empty());
+
+		Assertions.assertThrows(ItemNotFoundException.class, () -> service.getRequestByID(1L, 1L));
+	}
+
+	@Test
+	void checkGetRequestByIDSuccess() {
+		when(userRepository.findById(any())).thenReturn(Optional.of(firstUser));
+		when(itemRequestRepository.findById(any())).thenReturn(Optional.of(itemRequest));
+		when(itemRepository.findAllByRequestIdOrderByIdAsc(any())).thenReturn(List.of(item));
+		when(mapper.requestToResponse(any())).thenReturn(itemRequestResponseDto);
+
+		Assertions.assertEquals(service.getRequestByID(3L, 1L).getId(), 1);
+	}
+
+	@Test
+	void checkGetRequestListByPagesThrowException() {
+		when(userRepository.findById(any())).thenReturn(Optional.empty());
+
+		Assertions.assertThrows(ItemNotFoundException.class, () -> service.getRequestListByPages(1L, 1, 10));
+	}
+}
